207ab2014f9035df6636a09c78fa8f7bb847beee
14deepak22-eng <14deepak22@gmail.com>
2026-07-27 17:42:37 +0530

    Update announcement.ts

--- imi-pgdm-smart-schedule/lib/announcement.ts
+++ imi-pgdm-smart-schedule/lib/announcement.ts
@@ -23,5 +23,5 @@ export const ANNOUNCEMENT = {
   message: 'Due to an incorrect year entry in the Google Sheet, classes for 27th July are not showing on the dashboard. Please refer to the Google Sheet directly for 27th July schedule -Deepak Kumar.',
   // Example: live from right now until end of day tomorrow (IST).
   startAt: '2026-07-26T00:00:00+05:30' as string | null,
-  endAt: '2026-07-27T23:59:59+05:30' as string | null,
+  endAt: '2026-07-27T17:59:59+05:30' as string | null,
 };
