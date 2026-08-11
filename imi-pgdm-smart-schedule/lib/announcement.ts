8e8029e56167b58bab923bdc6e2b356d2f50b2bb
14deepak22-eng <14deepak22@gmail.com>
2026-08-11 18:07:28 +0530

    Update announcement.ts

--- imi-pgdm-smart-schedule/lib/announcement.ts
+++ imi-pgdm-smart-schedule/lib/announcement.ts
@@ -41,7 +41,7 @@ export const ANNOUNCEMENT = {
   message:`Just search "IMI PGDM Smart Schedule" on Google and you can find the website directly. 🌐
 No need to search for the link separately anymore! 😄`,
   videoUrl: null as string | null,
-  imageUrl: "/announcements/WhatsApp Image 2026-08-10 at 8.20.09 PM.jpeg" as string | null,
+  imageUrl: "/announcements/logo imi.jpeg" as string | null,
   // Example: live from right now until end of day tomorrow (IST).
   startAt: "2026-08-10T00:00:00+05:30" as string | null,
   endAt: "2026-08-11T17:59:59+05:30" as string | null,
